45d19bbdb5393532fcc4a3976783abf7499ccb60
Vishal R <vishalramesh01@gmail.com>
2020-11-06 10:37:06 +0530

    Create HotelSchema, Bug Fixes in Google Authentication, Add Support for MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
-const findOrCreate = require("mongoose-findorcreate");
 const { OAuth2Client } = require("google-auth-library");
-const { response } = require("express");
 const jwt = require("jsonwebtoken");
 
 const app = express();
@@ -25,31 +19,303 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //MongoDB code below
 mongoose.connect(
-  "mongodb://localhost:27017/userDB",
+  "mongodb://localhost:27017/petraDB",
   {
-    useUnifiedTopology: true,
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   },
   function (err) {
     if (err) console.log("MongoDB Connection Error!");
-    else console.log("Connected to MongoDB!");
+    else console.log("Connected to PeTra MongoDB!");
   }
 );
-mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
-  tokenId: String,
-  googleId: String,
-  name: String,
-  email: String,
-  imageUrl: String,
-  isVerified: Boolean,
-  perks: Number,
+  _id: {
+    type: String,
+  },
+  tokenId: {
+    type: String,
+  },
+  googleId: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  imageUrl: {
+    type: String,
+  },
+  perks: {
+    type: String,
+  },
 });
 
-userSchema.plugin(passportLocalMongoose);
+const hotelListSchema = new mongoose.Schema({
+  img: String,
+  location: String,
+  title: String,
+  description: String,
+  star: Number,
+  price: String,
+  total: String,
+  hotelID: Number,
+  coordinates: {
+    latitude: Number,
+    longitude: Number,
+  },
+  photos: [
+    {
+      img: String,
+    },
+  ],
+});
 
-const User = new mongoose.model("User", userSchema);
+const hotelSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+  },
+  City: String,
+  State: String,
+  Country: String,
+  CityLocation: {
+    latitude: Number,
+    longitude: Number,
+  },
+  hotelList: [hotelListSchema],
+});
+
+const User = new mongoose.model("PetraUser", userSchema);
+
+const Hotel = new mongoose.model("Hotel", hotelSchema);
+
+// newHotel = new Hotel({
+//   _id: "INDKABLR",
+//   City: "Bangalore",
+//   State: "Karnataka",
+//   Country: "India",
+//   CityLocation: {
+//     latitude: 12.971599,
+//     longitude: 77.594566,
+//   },
+//   hotelList: [
+//     {
+//       img:
+//         "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//       location: "Bangalore Central",
+//       title: "Stay at this spacious Edwardian House",
+//       description:
+//         "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+//       star: 4.73,
+//       price: "£30 / night",
+//       total: "£117 total",
+//       hotelID: 1,
+//       coordinates: {
+//         latitude: 12.971599,
+//         longitude: 77.594566,
+//       },
+//       photos: [
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//       ],
+//     },
+//     {
+//       img:
+//         "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
+//       location: "Kattriguppe",
+//       title: "Independant luxury studio apartment",
+//       description:
+//         "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
+//       star: 4.3,
+//       price: "£40 / night",
+//       total: "£157 total",
+//       hotelID: 2,
+//       coordinates: {
+//         latitude: 12.933435,
+//         longitude: 77.557832,
+//       },
+//       photos: [
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//       ],
+//     },
+//     {
+//       img:
+//         "https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
+//       location: "Jayanagar 4th Block",
+//       title: "London Studio Apartments",
+//       description:
+//         "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
+//       star: 3.8,
+//       price: "£35 / night",
+//       total: "£207 total",
+//       hotelID: 3,
+//       coordinates: {
+//         latitude: 12.92594,
+//         longitude: 77.583038,
+//       },
+//       photos: [
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//       ],
+//     },
+//     {
+//       img:
+//         "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
+//       location: "PES University",
+//       title: "Independant luxury studio apartment",
+//       description:
+//         "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
+//       star: 4.3,
+//       price: "£40 / night",
+//       total: "£157 total",
+//       hotelID: 4,
+//       coordinates: {
+//         latitude: 12.935641,
+//         longitude: 77.535857,
+//       },
+//       photos: [
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//       ],
+//     },
+//     {
+//       img:
+//         "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//       location: "UB City",
+//       title: "Stay at this spacious Edwardian House",
+//       description:
+//         "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+//       star: 4.73,
+//       price: "£30 / night",
+//       total: "£117 total",
+//       hotelID: 5,
+//       coordinates: {
+//         latitude: 12.9716418,
+//         longitude: 77.5955194,
+//       },
+//       photos: [
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//       ],
+//     },
+//     {
+//       img:
+//         "https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
+//       location: "Brigade Millenium",
+//       title: "London Studio Apartments",
+//       description:
+//         "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
+//       star: 3.8,
+//       price: "£35 / night",
+//       total: "£207 total",
+//       hotelID: 6,
+//       coordinates: {
+//         latitude: 12.8906899,
+//         longitude: 77.5804962,
+//       },
+//       photos: [
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//         {
+//           img:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+//         },
+//       ],
+//     },
+//   ],
+// });
+
+// Hotel.insertMany([newHotel], function (err, doc) {
+//   if (err) console.log("Hotel did not get inserted!");
+//   else console.log("Hotel got inserted into Hotel collection of PetraDB!");
+// });
+
 const client = new OAuth2Client(
   "1065157938718-eudu1eo9ic1l7dduroe3n85ffdthk9fp.apps.googleusercontent.com"
 );
@@ -60,6 +326,7 @@ app.get("/", function (req, res) {
 
 app.post("/auth/google/account/", function (req, res) {
   //Successfully Authenticated
+  console.log(req.body);
   const { tokenId, googleId, imageUrl } = req.body;
   client
     .verifyIdToken({
@@ -70,7 +337,7 @@ app.post("/auth/google/account/", function (req, res) {
     .then((response) => {
       const { email_verified, name, email } = response.payload;
       if (email_verified) {
-        User.findOne({ email }).exec((err, user) => {
+        User.findOne({ email: email }, async function (err, user) {
           if (err) {
             return res.status(400).json({
               error: "Something Went Wrong!",
@@ -88,23 +355,21 @@ app.post("/auth/google/account/", function (req, res) {
                 user: { _id, name, email, imageUrl, perks },
               });
             } else {
-              let newUser = new User({
+              newUser = new User({
+                _id: googleId,
                 tokenId: tokenId,
                 googleId: googleId,
                 name: name,
                 email: email,
                 imageUrl: imageUrl,
-                isVerified: true,
-                perks: 1000,
+                perks: "1000",
               });
-              newUser.save((err, data) => {
-                if (err) {
-                  return res.status(400).json({
-                    error: "Something Went Wrong!",
-                  });
-                } else {
+              User.insertMany([newUser], function (err, user) {
+                if (err) console.log(err);
+                else console.log("User has been registered!");
+                if (user) {
                   const token = jwt.sign(
-                    { _id: data._id },
+                    { _id: user._id },
                     "This is a secret that we will keep it as a secret!",
                     { expiresIn: "7d" }
                   );
@@ -112,6 +377,10 @@ app.post("/auth/google/account/", function (req, res) {
                   res.json({
                     token,
                     user: { _id, name, email, imageUrl, perks },
+                  });
+                } else {
+                  return res.status(400).json({
+                    error: "Something Went Wrong!",
                   });
                 }
               });
@@ -125,176 +394,14 @@ app.post("/auth/google/account/", function (req, res) {
 app.post("/auth/google/account/search/", function (req, res) {
   console.log(req.body);
   res.send({
-    NumberOfResults: 10,
-    Source: "Data obtained from MONGODB",
-    results: [
-      {
-        img:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
-        location: "Bangalore Central",
-        title: "Stay at this spacious Edwardian House",
-        description:
-          "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
-        star: 4.73,
-        price: "£30 / night",
-        total: "£117 total",
-        hotelID: 1,
-        coordinates: [12.971599, 77.594566],
-      },
-      {
-        img:
-          "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
-        location: "Kattriguppe",
-        title: "Independant luxury studio apartment",
-        description:
-          "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
-        star: 4.3,
-        price: "£40 / night",
-        total: "£157 total",
-        hotelID: 2,
-        coordinates: [12.933435, 77.557832],
-      },
-      {
-        img:
-          "https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
-        location: "Jayanagar 4th Block",
-        title: "London Studio Apartments",
-        description:
-          "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
-        star: 3.8,
-        price: "£35 / night",
-        total: "£207 total",
-        hotelID: 3,
-        coordinates: [12.92594, 77.583038],
-      },
-      {
-        img:
-          "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
-        location: "PES University",
-        title: "Independant luxury studio apartment",
-        description:
-          "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
-        star: 4.3,
-        price: "£40 / night",
-        total: "£157 total",
-        hotelID: 4,
-        coordinates: [12.935641, 77.535857],
-      },
-      {
-        img:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
-        location: "UB City",
-        title: "Stay at this spacious Edwardian House",
-        description:
-          "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
-        star: 4.73,
-        price: "£30 / night",
-        total: "£117 total",
-        hotelID: 5,
-        coordinates: [12.9716418, 77.5955194],
-      },
-      {
-        img:
-          "https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
-        location: "Brigade Millenium",
-        title: "London Studio Apartments",
-        description:
-          "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
-        star: 3.8,
-        price: "£35 / night",
-        total: "£207 total",
-        hotelID: 6,
-        coordinates: [12.8906899, 77.5804962],
-      },
-    ],
+    results: [],
   });
 });
 
 app.post("/search/", function (req, res) {
   console.log(req.body);
   res.send({
-    NumberOfResults: 10,
-    Source: "Data obtained from MONGODB",
-    results: [
-      {
-        img:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
-        location: "Bangalore Central",
-        title: "Stay at this spacious Edwardian House",
-        description:
-          "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
-        star: 4.73,
-        price: "£30 / night",
-        total: "£117 total",
-        hotelID: 1,
-        coordinates: [12.971599, 77.594566],
-      },
-      {
-        img:
-          "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
-        location: "Kattriguppe",
-        title: "Independant luxury studio apartment",
-        description:
-          "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
-        star: 4.3,
-        price: "£40 / night",
-        total: "£157 total",
-        hotelID: 2,
-        coordinates: [12.933435, 77.557832],
-      },
-      {
-        img:
-          "https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
-        location: "Jayanagar 4th Block",
-        title: "London Studio Apartments",
-        description:
-          "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
-        star: 3.8,
-        price: "£35 / night",
-        total: "£207 total",
-        hotelID: 3,
-        coordinates: [12.92594, 77.583038],
-      },
-      {
-        img:
-          "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
-        location: "PES University",
-        title: "Independant luxury studio apartment",
-        description:
-          "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
-        star: 4.3,
-        price: "£40 / night",
-        total: "£157 total",
-        hotelID: 4,
-        coordinates: [12.935641, 77.535857],
-      },
-      {
-        img:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
-        location: "UB City",
-        title: "Stay at this spacious Edwardian House",
-        description:
-          "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
-        star: 4.73,
-        price: "£30 / night",
-        total: "£117 total",
-        hotelID: 5,
-        coordinates: [12.9716418, 77.5955194],
-      },
-      {
-        img:
-          "https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
-        location: "Brigade Millenium",
-        title: "London Studio Apartments",
-        description:
-          "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
-        star: 3.8,
-        price: "£35 / night",
-        total: "£207 total",
-        hotelID: 6,
-        coordinates: [12.8906899, 77.5804962],
-      },
-    ],
+    results: [],
   });
 });
 
